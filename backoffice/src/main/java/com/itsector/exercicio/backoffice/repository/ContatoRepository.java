1362513451f4aa065615b6c63f30c1ac6ac41d4e
aogasapp <aogasapp@gmail.com>
2019-12-11 21:53:04 +0000

    Removido diretorio duplicado

--- backoffice/src/main/java/com/itsector/exercicio/backoffice/repository/ContatoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itsector.exercicio.backoffice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.itsector.exercicio.backoffice.model.Contato;
-
-public interface ContatoRepository extends JpaRepository<Contato, Long> {
-
-}
